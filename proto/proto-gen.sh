4d6527d847c4b49ba61d5c196ccbce5ff8ffea4a
benleong0 <benleong@myurbanjungle.com>
2023-01-02 11:53:41 +0000

    python protogen

--- proto/proto-gen.sh
+++ proto/proto-gen.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 yarn proto-loader-gen-types --grpcLib=@grpc/grpc-js --outDir=proto-ts/ proto/*.proto;
-
 cp -r proto-ts mastercard-service/proto-types;
-rm -rf proto-ts
+rm -rf proto-ts;
+
+mkdir proto-py;
+python -m grpc_tools.protoc -I . --python_out=proto-py --pyi_out=proto-py --grpc_python_out=proto-py proto/*.proto;
+cp -r proto-py/proto tokenisation-service/proto-types;
+rm -rf proto-py;
