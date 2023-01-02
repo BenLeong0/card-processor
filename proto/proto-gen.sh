d88a4bf20c83941fdde3e776b7f798f4239a666b
benleong0 <benleong@myurbanjungle.com>
2023-01-02 12:19:50 +0000

    fix script

--- proto/proto-gen.sh
+++ proto/proto-gen.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
 yarn proto-loader-gen-types --grpcLib=@grpc/grpc-js --outDir=proto-ts/ proto/*.proto;
-cp -r proto-ts mastercard-service/proto-types;
+
+# Copy to all node services here
+cp -rT proto-ts mastercard-service/proto-types;
 rm -rf proto-ts;
+
 
 mkdir proto-py;
 python -m grpc_tools.protoc -I . --python_out=proto-py --pyi_out=proto-py --grpc_python_out=proto-py proto/*.proto;
-cp -r proto-py/proto tokenisation-service/proto-types;
+
+# Copy to all python services here
+cp -rT proto-py/proto/ tokenisation-service/proto-types;
+cp -rT proto-py/proto/ hsm-service/proto-types;
 rm -rf proto-py;
